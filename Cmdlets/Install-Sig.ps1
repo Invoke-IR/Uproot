46dd6a8404f8aa50851246bb1b413ce9c430e012
sixdub <justin@sixdub.net>
2015-08-18 17:11:53 -0400

    Added ability to deploy prebuilt sigs

---
+++ Cmdlets/Install-Sig.ps1
@@ -0,0 +1,67 @@
+﻿function Install-Sig {
+[CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $False, ValueFromPipeline = $True)]
+            [string[]]$ComputerName = '.'
+    )
+    DynamicParam {
+        # Set the dynamic parameters' name
+        $ParameterName = 'SigFile'
+            
+        # Create the dictionary 
+        $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+
+        # Create the collection of attributes
+        $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+            
+        # Create and set the parameters' attributes
+        $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+        $ParameterAttribute.Mandatory = $true
+        $ParameterAttribute.ParameterSetName = 'SigFile'
+
+        # Add the attributes to the attributes collection
+        $AttributeCollection.Add($ParameterAttribute)
+
+        # Generate and set the ValidateSet 
+        $arrSet = (Get-ChildItem -Path "$($UprootPath)\Sigs").BaseName
+        $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
+
+        # Add the ValidateSet to the attributes collection
+        $AttributeCollection.Add($ValidateSetAttribute)
+
+        # Create and return the dynamic parameter
+        $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection)
+        $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
+        return $RuntimeParameterDictionary
+    }
+     BEGIN
+    {
+        $SigFile = $PSBoundParameters['SigFile']
+    }
+
+    PROCESS
+    {
+        foreach($computer in $ComputerName)
+        {
+            if($PSBoundParameters.ContainsKey("SigFile")){
+                Get-Content "$($UprootPath)\Sigs\$($SigFile).ps1" | Out-String | Invoke-Expression
+                foreach($f in $filters){
+                    Add-WmiEventFilter -ComputerName $computer -FilterFile $f 
+                }
+
+                foreach($c in $consumers){
+                    Add-WmiEventConsumer -ComputerName $computer -ConsumerFile $c
+                }
+
+                $subscriptions.GetEnumerator() | foreach-object{
+                    Add-WmiEventSubscription -ComputerName $computer -FilterName $_.Name -ConsumerName $_.Value
+                }
+            }
+
+            else
+            {
+                Write-Error "Require Sig File"
+            }
+        }
+    }
+}
---
+++ Sigs/Basic.ps1
@@ -0,0 +1,23 @@
+﻿$filters = @(
+    "UserCreation",
+    "LoggedOnUserCreation",
+    "ProcessCreation",
+    "ProcessStartTrace",
+    "ServerConnectionCreation",
+    "ServiceCreation"
+)
+
+$consumers = @(
+    "AS_IntrinsicHTTPPost",
+    "AS_ExtrinsicHTTPPost"
+)
+
+$subscriptions =@{
+    "UserCreation" = "AS_IntrinsicHTTPPost";
+    "LoggedOnUserCreation"="AS_IntrinsicHTTPPost";
+    "ProcessCreation"="AS_IntrinsicHTTPPost";
+    "ProcessStartTrace"="AS_ExtrinsicHTTPPost";
+    "ServerConnectionCreation"="AS_IntrinsicHTTPPost";
+    "ServiceCreation"="AS_IntrinsicHTTPPost";
+}
+
