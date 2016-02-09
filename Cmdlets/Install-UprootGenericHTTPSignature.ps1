c18ffed288c7e19cab5f712535ef30211fcf17ad
Harmj0y <will@harmj0y.net>
2016-02-09 15:48:04 -0500

    Added Install-UprootGenericHTTPSignature and Install-UprootGenericSignature for generic signature creation.

---
+++ Cmdlets/Install-UprootGenericHTTPSignature.ps1
@@ -0,0 +1,78 @@
+function Install-UprootGenericHTTPSignature {
+[CmdletBinding()]
+    Param
+    (
+        [Parameter()]
+        [Microsoft.Management.Infrastructure.CimSession[]]
+        $CimSession,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $FilterName,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $FilterNamespace = 'root/cimv2',
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $FilterQuery,
+
+        [Parameter()]
+        [Int32]
+        $ThrottleLimit = 32
+    )
+
+    process
+    {
+        if($PSBoundParameters['CimSession'])
+        {
+            # build the properties for our custom-specified filter and create the new filter
+            $FilterProps = @{
+                Name = $FilterName;
+                EventNamespace = $FilterNamespace;
+                Query = $FilterQuery;
+                QueryLanguage = 'WQL';
+                CimSession = $CimSession;
+                ThrottleLimit = $ThrottleLimit;
+            }
+            New-WmiEventFilter @FilterProps
+
+            # the consumer is always going to be static for this script
+            . "$($UprootPath)\Consumers\AS_GenericHTTP.ps1"
+            New-WmiEventConsumer @props -CimSession $CimSession -ThrottleLimit $ThrottleLimit -ErrorAction SilentlyContinue
+            
+            New-WmiEventSubscription -CimSession $CimSession -ConsumerType ActiveScriptEventConsumer -FilterName $FilterName -ConsumerName 'AS_GenericHTTP' -ErrorVariable err
+            $err | ForEach-Object {
+                Write-Warning "Error in executing New-WmiEventSubscription : $_"
+            }
+        }
+        else
+        {
+            # if there's no CimSession, we're deploying the signature to the local system
+
+            # build the properties for our custom-specified filter and create the new filter
+            $FilterProps = @{
+                'Name' = $FilterName;
+                'EventNamespace' = $FilterNamespace;
+                'Query' = $FilterQuery;
+                'QueryLanguage' = 'WQL';
+                'ComputerName' = 'localhost';
+                'ThrottleLimit' = $ThrottleLimit;
+            }
+            New-WmiEventFilter @FilterProps
+
+            # the consumer is always going to be static for this script
+            . "$($UprootPath)\Consumers\AS_GenericHTTP.ps1"
+            New-WmiEventConsumer @props -ComputerName 'localhost' -ErrorAction SilentlyContinue
+            
+            New-WmiEventSubscription -ConsumerType ActiveScriptEventConsumer -FilterName $FilterName -ConsumerName 'AS_GenericHTTP' -ComputerName 'localhost' -ErrorVariable err
+            $err | ForEach-Object {
+                Write-Warning "Error in executing New-WmiEventSubscription : $_"
+            }
+        }
+    }
+}
---
+++ Cmdlets/Install-UprootGenericSignature.ps1
@@ -0,0 +1,53 @@
+function Install-UprootGenericSignature {
+[CmdletBinding()]
+    Param
+    (
+        [Parameter()]
+        [Microsoft.Management.Infrastructure.CimSession[]]
+        $CimSession,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $FilterName,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $FilterNamespace = 'root/cimv2',
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $FilterQuery,
+
+        [Parameter()]
+        [Int32]
+        $ThrottleLimit = 32,
+
+        [Parameter(Mandatory)]
+        [ValidateSet("HTTP","EventLog")]
+        [String]
+        $Consumer
+    )
+
+    process
+    {
+        if($Consumer -eq 'HTTP')
+        {
+            $Arguments = @{
+                CimSession = $CimSession;
+                FilterName = $FilterName;
+                FilterNamespace = $FilterNamespace;
+                FilterQuery = $FilterQuery;
+                ThrottleLimit = $ThrottleLimit;
+            }
+            Install-GenericHTTPSignature @Arguments
+        }
+        elseif($Consumer -eq 'EventLog')
+        {
+            # TODO: implement New-GenericEventLogSignature
+            Throw "New-GenericEventLogSignature not yet implemented!"
+        }
+    }
+}
